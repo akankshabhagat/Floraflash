fe9c6ab8a7f46d9e32555a8ba00f6a48baf97312
akanksha <akankshabhagat606@gmail.com>
2024-04-08 21:18:47 +0530

    removed unused trial comments

--- plants.py
+++ plants.py
@@ -32,34 +32,3 @@ if __name__ == '__main__':
 
 
 
-# import requests
-# import json
-# from flask import Flask, render_template
-
-# key="NNx5gCUuS5viaKORm4xQ_3geyk4J8xE4n0DtKDXPJME"
-# r = requests.get('https://trefle.io/api/v1/species?filter%5Bflower_color%5D=red&token='+key)
-# # if r.status_code == 200:
-#     # Decode the response content from bytes to string
-
-
-
-# # response_data = r.content.decode('utf-8')
-# # #     # Convert the string to a Python dictionary
-# # data_dict = json.loads(response_data)
-# #     print(json.dumps(data_dict, indent=4))
-
-# flowername=r.json()['data'][7]['common_name']
-# flower=r.json()['data'][7]['image_url']
-  
-# # print(json.dumps(flower,indent=4))
-# plants = Flask(__name__)
-
-# @plants.route('/')
-# def index():
-#     heading=flowername
-    
-#    # Replace this with the URL of your image
-#     return render_template('index.html',heading=heading,image_url=flower)
-
-# if __name__ == '__main__':
-#     plants.run(debug=True)
